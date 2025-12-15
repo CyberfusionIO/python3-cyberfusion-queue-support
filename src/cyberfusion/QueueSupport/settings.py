5211a7049213a25a6776ee9fc1b3800a2cc48d6d
William David Edwards <wedwards@cyberfusion.nl>
2025-12-15 16:54:55 +0100

    Ignore extraneous config options
    This package is used as a library in other projects. In such projects, for local development, an `.env` file may exist with settings for both `QueueSupport` and the project at hand.
    
    This commit fixes Pydantic complaining about unknown settings - corresponding to the project using this library - by ignoring them, as was the default behaviour in Pydantic v1.

--- src/cyberfusion/QueueSupport/settings.py
+++ src/cyberfusion/QueueSupport/settings.py
@@ -3,7 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_prefix="queue_support_", env_file=(".env", "/etc/queue-support.conf")
+        env_prefix="queue_support_",
+        env_file=(".env", "/etc/queue-support.conf"),
+        extra="ignore",
     )
 
     database_path: str = "sqlite:///./queue-support.db"
